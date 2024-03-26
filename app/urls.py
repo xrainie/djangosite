4bdfdc8e5cb112be1b53ddef20bf0f032d0fcc88
xrainie <xoxrainie@gmail.com>
2024-03-26 23:11:53 +0300

    added goods app, connected catalog & product html

--- app/urls.py
+++ app/urls.py
@@ -14,12 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from argparse import Namespace
 from django.contrib import admin
 from django.urls import include, path
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls', namespace='main'))
+    path('', include('main.urls', namespace='main')),
+    path('catalog/', include('goods.urls', namespace='catalog'))
 ]
