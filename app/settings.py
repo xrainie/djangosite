1d4aff4b775eaae09ba7c9a0bca5dfa613cdb30d
xrainie <xoxrainie@gmail.com>
2024-04-03 01:57:09 +0300

    Postgesql database connected & added fullsearch

--- app/settings.py
+++ app/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.postgres',
 
     'main',
     'goods',
@@ -82,8 +83,12 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'home',
+        'USER': 'home',
+        'PASSWORD': 'home',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
--- goods/utils.py
+++ goods/utils.py
@@ -1,5 +1,6 @@
 from django.db.models import Q
 from goods.models import Products
+from django.contrib.postgres.search import SearchVector
 
 
 def q_search(query):
@@ -7,12 +8,16 @@ def q_search(query):
   if query.isdigit() and len(query) <= 5:
     return Products.objects.filter(id=int(query))
   
-  keywords = [word for word in query.split() if len(word) > 2]
+  return Products.objects.annotate(search=SearchVector('name', 'description')).filter(search=query)
+  
+  # return Products.objects.filter(description__search=query)
+  
+  # keywords = [word for word in query.split() if len(word) > 2]
 
-  q_objects = Q()
+  # q_objects = Q()
 
-  for token in keywords:
-    q_objects |= Q(description__icontains=token)
-    q_objects |= Q(name__icontains=token)
+  # for token in keywords:
+  #   q_objects |= Q(description__icontains=token)
+  #   q_objects |= Q(name__icontains=token)
 
-  return Products.objects.filter(q_objects)
+  # return Products.objects.filter(q_objects)
